a0a027e4ef8a3294c62d510440f4bc2be7f2460f
shakeib98 <shakeib98@outlook.com>
2020-12-29 16:43:05 +0500

    two lessons completed

---
+++ data-types.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	rune1 := []rune("s")
+	fmt.Printf("%v", rune1)
+}
---
+++ var-keyword.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var i, j, k bool
+
+var s, t string = "Hi", "hello"
+
+func main() {
+	var l int
+	fmt.Println(i, j, k, l)
+	fmt.Println(s, t)
+}
