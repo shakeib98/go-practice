ab3d6eabf140e7f12e0ed0c29d790f60a828855b
shakeib98 <shakeib98@outlook.com>
2020-12-30 00:16:50 +0500

    newton square root completed

---
+++ if-statement.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	if v := math.Pow(2, 2); v > 2 {
+		fmt.Println("Square root is bigger")
+	}
+}
---
+++ newton-square-root.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func Sqrt(x float64) float64 {
+	z := x / 2
+	// Xn - f(Xn)/f'(Xn) -> x^2 - 2 - > 2x -> Xn - (Xn*Xn - x)/2*Xn
+	for {
+		temp := z - ((z*z - x) / (2 * z))
+		if (z-temp > 0 && z-temp < 0.0001) || z == temp {
+			return temp
+		}
+		z = temp
+
+	}
+
+}
+
+func main() {
+	fmt.Println(Sqrt(4))
+}
