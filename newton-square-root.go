9093b1aa137700bd76118fc3077cd282b4123201
shakeib98 <shakeib98@outlook.com>
2021-06-29 20:00:30 +0500

    added pointers in struct file

--- newton-square-root.go
+++ newton-square-root.go
@@ -1,20 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Sqrt(x float64) float64 {
-	z := x / 2
 	// Xn - f(Xn)/f'(Xn) -> x^2 - 2 - > 2x -> Xn - (Xn*Xn - x)/2*Xn
+	z := x / 2
 	for {
 		temp := z - ((z*z - x) / (2 * z))
-		if (z-temp > 0 && z-temp < 0.0001) || z == temp {
+		if z-temp < 0.0001 {
 			return temp
 		}
 		z = temp
-
 	}
 }
 
 func main() {
-	fmt.Println(Sqrt(4))
+	fmt.Println(Sqrt(16))
 }
